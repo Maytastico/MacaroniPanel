df18444f5094ba4f785df68a5a9895baa18a478e
MacaroniDamage <daniel.tohmas@hotmail.com>
2020-02-19 21:35:28 +0100

    some weird stuff

--- dashboard/admin.php
+++ dashboard/admin.php
@@ -12,12 +12,48 @@ if($a->verifySession()===false){
     header("Location: ../login.php");
 }
 
+
+/**Get Values**/
+$getPage = !empty($_GET['page']) ? $_GET['page'] : null;
+if($getPage === null){
+    header("Location: ?site=1&page=users");
+}
 $getUid = !empty($_GET['uid']) ? $_GET['uid'] : null ;
 $getEmail = !empty($_GET['email']) ? $_GET['email'] : null ;
-$getInstall = !empty($_GET['install']) ? $_GET['install'] : null;
-$getreinstall = !empty($_GET['reinstall']) ? $_GET['reinstall'] : null ;
-$getSignup = !empty($_GET['signup']) ? $_GET['signup'] : null ;
 $getRoleModel = !empty($_GET['roleModel']) ? $_GET['roleModel'] : null ;
+
+
+$getSite = !empty($_GET['site']) ? $_GET['site'] : null ;
+if($getSite === null){
+    header("Location: ?site=1&page=$getPage");
+}
+$getMax = !empty($_GET['maxEntries']) ? $_GET['maxEntries'] : null;
+
+/**Post Values**/
+$postMaxEntries = !empty($_POST['maxEntries']) ? $_POST['maxEntries'] : 20;
+$postNextSite = isset($_POST['nextSite']);
+$postLastSite = isset($_POST['lastSite']);
+$postSearch = !empty($_POST['search']) ? $_POST['search'] : null;
+$postPage = !empty($_POST['page']) ? $_POST['page'] : null;
+/**Defining Datatypes**/
+settype($getSite, "Integer");
+settype($postMaxEntries, "Integer");
+
+/*Global header*/
+$header = "?site=$getSite&page=$getPage";
+
+if($postNextSite === true){
+    $getSite = $getSite + 1;
+    header("Location:".$header);
+}else if ($postLastSite === true){
+    $getSite = $getSite - 1;
+    header("Location:".$header);
+}
+var_dump($postPage);
+if($postPage != null){
+    header("Location: ?site=$getSite&page=$postPage");
+}
+
 ?>
 
 <body id="admin">
@@ -27,13 +63,49 @@ $getRoleModel = !empty($_GET['roleModel']) ? $_GET['roleModel'] : null ;
                 <a href="index.php"><div class="icon"><img src="<?php echo Loader::$jump;?>/assets/icons/feather/skip-back.svg"></div></a>
             </div>
             <div class="middle">
-                <a href="?users"><div class="icon"><img src="<?php echo Loader::$jump;?>/assets/icons/feather/user.svg"> Users</div></a>
-                <a href="?modules"><div class="icon"><img src="<?php echo Loader::$jump;?>/assets/icons/feather/briefcase.svg"> Modules</div></a>
-                <a href="?permissions"><div class="icon"><img src="<?php echo Loader::$jump;?>/assets/icons/feather/users.svg"> Permissions</div></a>
+                <form action="admin.php?site=<?php echo $getSite . "&page=".$getPage?>" method="post">
+                    <button name="page" type="submit" value="users" class="small icon" ><div class="icon"><img src="<?php echo Loader::$jump;?>/assets/icons/feather/user.svg"> Users</div></button>
+                    <button name="page" type="submit" value="modules" class="small icon"><div class="icon"><img src="<?php echo Loader::$jump;?>/assets/icons/feather/briefcase.svg"> Modules</div></button>
+                    <button name="page" type="submit" value="permissions" class="small icon"><div class="icon"><img src="<?php echo Loader::$jump;?>/assets/icons/feather/users.svg"> Permissions</div></button>
+                </form>
             </div>
         </section>
     </nav>
     <section id="content">
+
+        <?php var_dump($postMaxEntries);?>
+        <form method="post" action="admin.php?site=<?php echo $getSite . "&page=".$getPage?>">
+            <div>
+                <input name="search">
+                <button>Search</button>
+            </div>
+            <div>
+                <?php if($getSite > 1)
+                    echo '<button class="small"  name="lastSite"><img class="invert" src="'.Loader::$jump.'/assets/icons/feather/arrow-left.svg"></button>'; ?>
+                <select name="maxEntries">
+                    <option <?php
+                        if($postMaxEntries==10)
+                            echo "selected"; ?>>10</option>
+
+                    <option<?php
+                        if($postMaxEntries==20)
+                            echo "selected";
+                    ?>>20</option>
+
+                    <option<?php
+                        if($postMaxEntries==50)
+                            echo "selected";
+                    ?>>50</option>
+
+                    <option<?php
+                        if($postMaxEntries==100)
+                            echo "selected";
+                    ?>>100</option>
+                </select>
+
+                <button class="small"  name="nextSite"><img class="invert" src="<?php echo Loader::$jump;?>/assets/icons/feather/arrow-right.svg"></button>
+            </div>
+        </form>
         <table class="tableContent">
             <tr>
                 <th>Profile</th>
@@ -58,7 +130,8 @@ $getRoleModel = !empty($_GET['roleModel']) ? $_GET['roleModel'] : null ;
             </tr>
         </table>
 
+        <?php
+            if($getPage)
+        ?>
     </section>
-    <pre
-
 </body>
